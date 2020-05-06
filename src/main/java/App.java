a650db4b39a4968755b1c2bce4a7ad3de503d745
hubdevelopers <3151411683@qq.com>
2020-05-06 22:29:43 +0800

    app提交

---
+++ src/main/java/App.java
@@ -0,0 +1,40 @@
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        // 1打包
+        System.out.println("欢迎各位同学来听课");
+        
+        // 2打包3
+        System.out.println("ddddddd");
+        
+        // 3打包3
+        System.out.println("ddddddd");
+        
+        
+        // 集成
+        // 1、持续集成 项目构建 === jar war
+        
+        // 2、手动启动  冷部署
+        // 3、手动n次 tomcat自动启动
+        
+        // 自动部署
+        
+        //  jenkins  == 持续集成，持续热部署
+        
+        // 1、手动
+        // 2、自动  GitHub web hooks   
+         //  2.1 回调
+         //  2.2 定时器
+        
+        
+        
+    }
+}
